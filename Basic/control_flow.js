35f5a739a50cab8268a57bf9242edac10fe541d3
Tushar14969 <130268128+Tushar14969@users.noreply.github.com>
2024-01-27 08:25:45 +0000

    map, filter and foreach

--- Basic/control_flow.js
+++ Basic/control_flow.js
@@ -33,3 +33,94 @@ map.set('five',5)
 for (const [key, value] of map) {
     // console.log(`${key} ${value}`);
 }
+
+// objects can not be iterated with the help of forof loop
+
+const obj = {
+    1:'one',
+    2:'two',
+    3:'three',
+    4:'four'
+}
+
+// iterating over an object
+for (const key in obj) {
+    // console.log(`${key} ${obj[key]}`);
+}
+
+let a = [1,2,3,4]
+// in forin loop for(const key in array/object) key stores the indexes
+for(value in a){
+    // console.log(a[value]);
+}
+
+// maps cannot be iterated by forin loop
+for(const i in map){
+    // console.log(map[i]);
+}
+
+a.forEach(function (it){
+    // console.log(it);
+})
+
+// we can also use arrow functions inside the parantheses
+// callback functions don't have a name
+// we can pass multiple parameters to the function like array index, values and an array itself
+// forEach returns nothing
+a.forEach((ite,index,a) => {
+    // console.log(`square ${ite ** 2} index ${index} whole array ${a}`);
+})
+
+// array with objects as its elements
+
+let objArray = [
+    {
+        Name:'Tushar',
+        Course:'MCA',
+        City:'Saharanpur'
+    },
+
+    {
+        Name:'Tarun',
+        Course:'BCA',
+        City:'Lucknow'
+    },
+
+    {
+        Name:'Kartik',
+        Course:'BCA',
+        City:'Deheradun'
+    },
+
+    {
+        Name:'Akash',
+        Course:'BBA',
+        City:'Meerut'
+    }
+]
+
+objArray.forEach((item) => {
+    // console.log(item.City);
+})
+
+// filter returns the values
+
+let n = a.filter( (i) => {
+    return i >= 2
+} )
+
+// console.log(typeof n);
+let nn = []
+a.forEach( (it) => {
+    if(it >= 2){
+        nn.push(it)
+    }
+} )
+
+// console.log(nn);
+
+let mapArray = a.map( (i) => {
+    return i * 10
+} )
+
+console.log(mapArray);
